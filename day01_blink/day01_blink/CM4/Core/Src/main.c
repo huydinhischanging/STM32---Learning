a2812e9f28a97f1a8e708a5f0ebaa791fecce156
Tu Dinh Huy <tuhuyne@gmail.com>
2026-09-16 16:10:06 +0700

    Day 1: GPIO output - blink LED_B on PD11

--- day01_blink/day01_blink/CM4/Core/Src/main.c
+++ day01_blink/day01_blink/CM4/Core/Src/main.c
@@ -120,6 +120,8 @@ int main(void)
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
+	    HAL_GPIO_TogglePin(LED_B_GPIO_Port, LED_B_Pin);
+	    HAL_Delay(500);
   }
   /* USER CODE END 3 */
 }
